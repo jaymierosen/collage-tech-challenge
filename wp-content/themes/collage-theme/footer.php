b275d07b9997f2bae216cb258961e587a91e85f1
Jaymie Rosen <rosen.jaymie@gmail.com>
2017-09-28 15:41:37 -0400

    font working -- need to start making ACF

--- wp-content/themes/collage-theme/footer.php
+++ wp-content/themes/collage-theme/footer.php
@@ -1,6 +1,25 @@
-<footer>
+<footer class="bg-zumthor">
 	<div class="container">
-		<p>&copy; Collage <?php echo date('Y'); ?></p>
+		<div class="clearfix my4 py3">
+			<div class="col-12 center">
+				<div class="h3 fw-l silla">Find out how all-in-one HR software can <br />help your business thrive.</div>
+				<div class="mt3"><a href="https://secure.collage.co/users/new/" target="_blank" class="Btn Btn--silla">Get Started for Free</a></div>
+			</div>
+		</div>
+		<div class="clearfix py3" style="border-top:1px solid rgba(62,105,233,0.21);">
+			<div class="col col-12 relative">
+				<div class="table clearfix full-width">
+					<div class="sm-block table-cell align-middle left-align sm-center list-reset">
+						<a href="/" class="inline-block"><img src="https://www.collage.co/wp-content/themes/collage/library/img/logo-dark@2x.png" height="30" class="inline-block"/></a>
+					</div>
+					<div class="sm-block table-cell align-middle right-align sm-center sm-mt2">
+						<div class="inline-block sm-mr2 mr3"><div class="h8 fw-l silla">Collage &copy; 2017</div></div>
+						<div class="inline-block sm-mr2 mr3"><a class="h8 fw-l silla" href="/privacy-policy/" target="_blank">Privacy Policy</a></div>
+						<div class="inline-block"><a class="h8 fw-l silla" href="/service-agreement/" target="_blank">Service Agreement</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </footer>
 <script>
--- wp-content/themes/collage-theme/functions.php
+++ wp-content/themes/collage-theme/functions.php
@@ -47,18 +47,18 @@ add_action( 'after_setup_theme', 'theme_setup' );
 We'll let WordPress add them to our templates automatically instead
 of writing our own link tags in the header. */
 
-function hackeryou_styles(){
+function collage_styles(){
 	wp_enqueue_style('style', get_stylesheet_uri() );
-
-	wp_enqueue_style('fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
+	wp_enqueue_style('basscss', 'https://unpkg.com/basscss@8.0.2/css/basscss.min.css');
+	// wp_enqueue_style('tablepress', '../plugins/tablepress/css/default.min.css');
 }
 
-add_action( 'wp_enqueue_scripts', 'hackeryou_styles');
+add_action( 'wp_enqueue_scripts', 'collage_styles');
 /* Add all our JavaScript files here.
 We'll let WordPress add them to our templates automatically instead
 of writing our own script tags in the header and footer. */
 
-function hackeryou_scripts() {
+function collage_scripts() {
 
 	//Don't use WordPress' local copy of jquery, load our own version from a CDN instead
 	wp_deregister_script('jquery');
@@ -87,12 +87,12 @@ function hackeryou_scripts() {
   );
 }
 
-add_action( 'wp_enqueue_scripts', 'hackeryou_scripts');
+add_action( 'wp_enqueue_scripts', 'collage_scripts');
 
 
 /* Custom Title Tags */
 
-function hackeryou_wp_title( $title, $sep ) {
+function collage_wp_title( $title, $sep ) {
 	global $paged, $page;
 
 	if ( is_feed() ) {
@@ -110,65 +110,65 @@ function hackeryou_wp_title( $title, $sep ) {
 
 	// Add a page number if necessary.
 	if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-		$title = "$title $sep " . sprintf( __( 'Page %s', 'hackeryou' ), max( $paged, $page ) );
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'collage' ), max( $paged, $page ) );
 	}
 
 	return $title;
 }
-add_filter( 'wp_title', 'hackeryou_wp_title', 10, 2 );
+add_filter( 'wp_title', 'collage_wp_title', 10, 2 );
 
 /*
   Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
  */
-function hackeryou_page_menu_args( $args ) {
+function collage_page_menu_args( $args ) {
 	$args['show_home'] = true;
 	return $args;
 }
-add_filter( 'wp_page_menu_args', 'hackeryou_page_menu_args' );
+add_filter( 'wp_page_menu_args', 'collage_page_menu_args' );
 
 
 /*
  * Sets the post excerpt length to 40 characters.
  */
-function hackeryou_excerpt_length( $length ) {
+function collage_excerpt_length( $length ) {
 	return 40;
 }
-add_filter( 'excerpt_length', 'hackeryou_excerpt_length' );
+add_filter( 'excerpt_length', 'collage_excerpt_length' );
 
 /*
  * Returns a "Continue Reading" link for excerpts
  */
-function hackeryou_continue_reading_link() {
+function collage_continue_reading_link() {
 	return ' <a href="'. get_permalink() . '">Continue reading <span class="meta-nav">&rarr;</span></a>';
 }
 
 /**
- * Replaces "[...]" (appended to automatically generated excerpts) with an ellipsis and hackeryou_continue_reading_link().
+ * Replaces "[...]" (appended to automatically generated excerpts) with an ellipsis and collage_continue_reading_link().
  */
-function hackeryou_auto_excerpt_more( $more ) {
-	return ' &hellip;' . hackeryou_continue_reading_link();
+function collage_auto_excerpt_more( $more ) {
+	return ' &hellip;' . collage_continue_reading_link();
 }
-add_filter( 'excerpt_more', 'hackeryou_auto_excerpt_more' );
+add_filter( 'excerpt_more', 'collage_auto_excerpt_more' );
 
 /**
  * Adds a pretty "Continue Reading" link to custom post excerpts.
  */
-function hackeryou_custom_excerpt_more( $output ) {
+function collage_custom_excerpt_more( $output ) {
 	if ( has_excerpt() && ! is_attachment() ) {
-		$output .= hackeryou_continue_reading_link();
+		$output .= collage_continue_reading_link();
 	}
 	return $output;
 }
-add_filter( 'get_the_excerpt', 'hackeryou_custom_excerpt_more' );
+add_filter( 'get_the_excerpt', 'collage_custom_excerpt_more' );
 
 
 /*
  * Register a single widget area.
  * You can register additional widget areas by using register_sidebar again
- * within hackeryou_widgets_init.
+ * within collage_widgets_init.
  * Display in your template with dynamic_sidebar()
  */
-function hackeryou_widgets_init() {
+function collage_widgets_init() {
 	// Area 1, located at the top of the sidebar.
 	register_sidebar( array(
 		'name' => 'Primary Widget Area',
@@ -182,23 +182,23 @@ function hackeryou_widgets_init() {
 
 }
 
-add_action( 'widgets_init', 'hackeryou_widgets_init' );
+add_action( 'widgets_init', 'collage_widgets_init' );
 
 /**
  * Removes the default styles that are packaged with the Recent Comments widget.
  */
-function hackeryou_remove_recent_comments_style() {
+function collage_remove_recent_comments_style() {
 	global $wp_widget_factory;
 	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
 }
-add_action( 'widgets_init', 'hackeryou_remove_recent_comments_style' );
+add_action( 'widgets_init', 'collage_remove_recent_comments_style' );
 
 
-if ( ! function_exists( 'hackeryou_posted_on' ) ) :
+if ( ! function_exists( 'collage_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post—date/time and author.
  */
-function hackeryou_posted_on() {
+function collage_posted_on() {
 	printf('<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s',
 		'meta-prep meta-prep-author',
 		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
@@ -215,11 +215,11 @@ function hackeryou_posted_on() {
 }
 endif;
 
-if ( ! function_exists( 'hackeryou_posted_in' ) ) :
+if ( ! function_exists( 'collage_posted_in' ) ) :
 /**
  * Prints HTML with meta information for the current post (category, tags and permalink).
  */
-function hackeryou_posted_in() {
+function collage_posted_in() {
 	// Retrieves tag list of current post, separated by commas.
 	$tag_list = get_the_tag_list( '', ', ' );
 	if ( $tag_list ) {
